4dd48cfa016a8d6a2d44897adf87a191c88a26e6
Jack <cheungsingyi@gmail.com>
2017-06-29 16:35:01 +0800

    differnt interpolate from react animation

--- index.android.js
+++ index.android.js
@@ -18,41 +18,105 @@ import {
 export default class animations extends Component {
   constructor () {
     super()
-    this.spinValue = new Animated.Value(0)
+    this.animatedValue = new Animated.Value(0)
   }
 
   componentDidMount() {
-    this.spin()
+    this.animate()
   }
 
-  spin() {
-    this.spinValue.setValue(0)
+  animate() {
+    this.animatedValue.setValue(0)
     Animated.timing(
-      this.spinValue,
+      this.animatedValue,
       {
         toValue: 1,
-        duration: 4000,
+        duration: 2000,
         easing: Easing.linear
       }
-    ).start(() => this.spin())
+    ).start(() => this.animate())
   }
   
  
   render() {
 
-    const spin = this.spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg']
-    })
+   const marginLeft = this.animatedValue.interpolate({
+     inputRange: [0, 1],
+     outputRange: [0, 300]
+   }) 
+
+   const opacity = this.animatedValue.interpolate({
+     inputRange: [0, 0.5, 1],
+     outputRange: [0, 1, 0]
+   })
+
+   const movingMargin = this.animatedValue.interpolate({
+     inputRange: [0, 0.5, 1],
+     outputRange: [0, 300, 0]
+   })
+
+   const textSize = this.animatedValue.interpolate({
+     inputRange: [0, 0.5, 1],
+     outputRange: [18, 32, 18]
+   })
+
+   const rotateX = this.animatedValue.interpolate({
+     inputRange: [0, 0.5, 1],
+     outputRange: ['0deg', '180deg', '0deg']
+   })
     return (
       <View style={styles.container}>
-        <Animated.Image
+        <Animated.View
            style={{
-             width: 227,
-             height: 200,
-             transform: [{rotate: spin}]}}
-             source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'}}
+             marginLeft,
+             height: 30,
+             width: 40,
+             backgroundColor: 'red'
+           }}
            />
+
+                <Animated.View
+           style={{
+             opacity,
+             marginTop:10,
+             height: 30,
+             width: 40,
+             backgroundColor: 'blue'
+           }}
+           />
+
+                <Animated.View
+           style={{
+             marginLeft: movingMargin,
+             marginTop: 10,
+             height: 30,
+             width: 40,
+             backgroundColor: 'orange'
+           }}
+           />
+
+                <Animated.Text
+           style={{
+             marginTop: 10,
+             fontSize: textSize,
+             height: 30,
+             width: 40,
+             backgroundColor: 'green'
+           }}
+           />
+
+                <Animated.View
+           style={{
+             transform:[{rotateX}],
+             marginLeft,
+             marginTop: 50,
+             height: 40,
+             width: 80,
+             backgroundColor: 'black'
+           }}
+           >
+          <Text style={{color: 'white'}}>Hello , i am jack</Text>
+           </Animated.View>
       </View>
     );
   }
@@ -61,9 +125,7 @@ export default class animations extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 150
   }
 });
 
