6ae0ae76e0a3a0386bd194d3462eda70cf04059c
Jack <cheungsingyi@gmail.com>
2017-06-29 16:12:09 +0800

    'react_animation'

---
+++ index.android.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native';
+
+export default class animations extends Component {
+  constructor () {
+    super()
+    this.spinValue = new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    this.spin()
+  }
+
+  spin() {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
+  
+ 
+  render() {
+
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+           style={{
+             width: 227,
+             height: 200,
+             transform: [{rotate: spin}]}}
+             source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'}}
+           />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('animations', () => animations);
